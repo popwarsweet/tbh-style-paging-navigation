25c1f3ab2c5b4873fd33cd43fd19e9e9a4755f20
Kyle Zaragoza <popwarsweet@gmail.com>
2022-04-24 15:29:23 +0200

    update swiftpm

--- Package.swift
+++ Package.swift
@@ -2,24 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "tbh-style-paging-navigation",
+    name: "PagingNavigation",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "PagingNavigation",
-            targets: ["PagingNavigation"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
+        .library(name: "PagingNavigation", targets: ["PagingNavigation"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "PagingNavigation",
-        ),
+        .target(name: "PagingNavigation")
     ]
 )
